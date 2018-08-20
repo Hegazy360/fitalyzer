54c0698fe949afe8cf5051b369fe24b220d0f017
Mohamed Hegazy <m.hegazy94@hotmail.com>
2018-08-20 15:02:51 +0200

    Started process to change MaterialUI to SemanticUI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import {Link} from "react-router-dom";
 import './css/Header.scss';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    display: 'flex',
-    flexGrow: 1,
-    justifyContent: 'space-between'
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
-
 function Header(props) {
-  const { classes } = props;
   return (
-    <div className={classes.root}>
-      <AppBar>
-        <Toolbar className={classes.flex}>
-          <Link to="/">
-            Fitalyzer
-          </Link>
-          <Link to="/gym">
-            Gym
-          </Link>
-        </Toolbar>
-      </AppBar>
+    <div class="ui top fixed menu">
+      <Link class="item" to="/">
+        Fitalyzer
+      </Link>
+      <Link class="item" to="/gym">
+        Gym
+      </Link>
     </div>
   );
 }
@@ -43,4 +20,4 @@ Header.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Header);
+export default Header;
