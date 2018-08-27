822b295ea01578539f85de77316f5723dd6b4d13
Mohamed Hegazy <m.hegazy94@hotmail.com>
2018-08-27 17:00:02 +0200

    Installed and initialized redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux"
+import store from "./redux/store"
 import './semantic/dist/semantic.min.css';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
