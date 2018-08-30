a0eff0685ceb90db90ab351c070fb149b03428c7
Mohamed Hegazy <m.hegazy94@hotmail.com>
2018-08-30 11:22:49 +0200

    Removed redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,8 @@ import logger from "redux-logger"
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 import reducer from "./reducers"
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middleware = applyMiddleware(promise(), thunk, logger)
-const store = createStore(reducer, composeWithDevTools(middleware))
+const store = createStore(reducer, middleware)
 
 export default store
